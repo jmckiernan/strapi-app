7979e2eab8c65245d0c3fc802047e256b3fde07e
John Mckiernan <jmckiernan86@gmail.com>
2025-03-11 01:08:06 -0700

    Fix: Enable Content-Type Builder in production

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,4 +9,10 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::content-type-builder',
+    config: {
+      enabled: true,
+    },
+  },
 ];
