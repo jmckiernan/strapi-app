a06e2a3ca3f1f958ac32d5d6e8d80a5f90927ca0
John Mckiernan <jmckiernan86@gmail.com>
2025-03-11 01:42:39 -0700

    Fix: Use default app keys for local development

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,8 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: [env('API_KEY_1'), env('API_KEY_2')],
+    keys: env('API_KEY_1') && env('API_KEY_2')
+      ? [env('API_KEY_1'), env('API_KEY_2')] // ✅ Use Render API keys in production
+      : ['localKey1', 'localKey2'], // ✅ Use local keys only in development
   },
 });
