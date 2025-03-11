5c406f00b47256270065ad2c7d3519b7de706279
John Mckiernan <jmckiernan86@gmail.com>
2025-03-10 23:46:19 -0700

    Fix: Use Render API keys for Strapi app security

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [env('API_KEY_1'), env('API_KEY_2')],
   },
 });
